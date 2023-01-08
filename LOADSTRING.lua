300ed85747a34cbc77cd7665348df626275852f7
Snomn <57030940+SnowHubv2@users.noreply.github.com>
2023-01-08 08:51:50 +0100

    New Loadstring

--- LOADSTRING.lua
+++ LOADSTRING.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHubv2/SnowHubv2/main/Main.lua"))()
