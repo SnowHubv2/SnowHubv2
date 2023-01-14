219c0b30a59953822384b2b8a487cc9288d791e8
Snomn <57030940+SnowHubv2@users.noreply.github.com>
2023-01-14 17:11:59 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,67 @@
-if not game:IsLoaded()then game.Loaded:Wait()end;local a=http_request or request or syn.request;if not a then game:GetService("StarterGui"):SetCore("SendNotification",{Title="Snow Hubv2 Error",Text="Your exploit is unsupported with Snow Hubv2!",Duration=5})return end;if makefolder and isfolder and not isfolder("Snow Hubv2")then makefolder("Snow Hubv2")makefolder("Snow Hubv2/Configs")makefolder("Snow Hubv2/Extra")end;if not isfile("/Snow Hubv2/Extra/Quotes_For_SnowHubv2.txt")then writefile("/Snow Hubv2/Extra/Quotes_For_SnowHubv2.txt",a({Url="https://raw.githubusercontent.com/Kitzoon/Rogue-Hub/main/Extra/Quotes.ROGUEHUB"}).Body)end;local a=a({Url="https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Games/"..game.PlaceId..".lua",Method="GET"})if a.StatusCode~=404 then game:GetService("Players").LocalPlayer.Idled:Connect(function()game:GetService("VirtualInputManager"):SendMouseButtonEvent(0,0,2,true,nil,0)wait(1)game:GetService("VirtualInputManager"):SendMouseButtonEvent(0,0,2,false,nil,0)end)repeat task.wait()until game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)~=nil;local b=queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport;if b and game.PlaceId~=6403373529 then b([[loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Main.lua", true))()]])end;loadstring(a.Body)()loadstring(game:HttpGet("https://pastebin.com/raw/pmGT8gpe"))()else game:GetService("StarterGui"):SetCore("SendNotification",{Title="Snow Hubv2 Error",Text="The game you are trying to play is not supported with Snow Hubv2!",Duration=5})loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Games/Universal.lua"))()loadstring(game:HttpGet("https://pastebin.com/raw/pmGT8gpe"))()return end
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+
+local req = http_request or request or syn.request
+
+
+if not req then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Snow Hubv2 Error",
+        Text = "Your exploit is unsupported with Snow Hubv2!",
+        Duration = 5
+    })
+    return
+end
+
+
+if makefolder and isfolder and not isfolder("Snow Hubv2") then
+    makefolder("Snow Hubv2")
+
+    makefolder("Snow Hubv2/Configs")
+    makefolder("Snow Hubv2/Extra")
+end
+
+
+if not isfile("/Snow Hubv2/Extra/Quotes_For_SnowHubv2.txt") then 
+    writefile("/Snow Hubv2/Extra/Quotes_For_SnowHubv2.txt", req({ Url = "https://raw.githubusercontent.com/Kitzoon/Rogue-Hub/main/Extra/Quotes.ROGUEHUB" }).Body);
+end
+
+
+local response = req({
+    Url = "https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Games/" .. game.PlaceId .. ".lua",
+    Method = "GET"
+})
+
+
+if response.StatusCode ~= 404 then
+    -- Anti AFK
+    game:GetService("Players").LocalPlayer.Idled:Connect(function()
+        game:GetService("VirtualInputManager"):SendMouseButtonEvent(0, 0, 2, true, nil, 0)
+        wait(1)
+        game:GetService("VirtualInputManager"):SendMouseButtonEvent(0, 0, 2, false, nil, 0)
+    end)
+    
+    repeat task.wait() until game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId) ~= nil
+    
+    local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
+
+    if teleportFunc and game.PlaceId ~= 6403373529 then
+        teleportFunc([[loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Main.lua", true))()]])
+    end
+
+    loadstring(game:HttpGet("https://pastebin.com/raw/pmGT8gpe"))()
+    loadstring(response.Body)()
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Snow Hubv2 Error",
+        Text = "The game you are trying to play is not supported with Snow Hubv2!",
+        Duration = 5
+    })
+
+
+    loadstring(game:HttpGet("https://pastebin.com/raw/pmGT8gpe"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SnowHack-Snomn/SnowHubv2/main/Games/Universal.lua"))()
+    return
+end
